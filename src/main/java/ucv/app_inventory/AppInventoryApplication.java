99d15189af68d61abdc9bb8714d9235561603748
Isaí Quiroga <isai.quiroga.2004@gmail.com>
2024-09-08 16:47:08 -0500

    Subida inicial de archivos del proyecto

---
+++ src/main/java/ucv/app_inventory/AppInventoryApplication.java
@@ -0,0 +1,13 @@
+package ucv.app_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInventoryApplication.class, args);
+	}
+
+}
